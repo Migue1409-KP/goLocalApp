b8e129df50944af19ca917fc10a128d27bc08e35
Miguel Mosquera <mopri38@gmail.com>
2025-06-02 17:06:52 -0500

    feature(guards): Add UserAuthGuard to manage user access based on authentication and role

---
+++ src/app/guards/user.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserAuthGuard implements CanActivate {
+  constructor(
+    private authService: AuthService, 
+    private router: Router
+  ) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot
+  ): boolean {
+    // Check if the user is logged in and has USER role
+    if (this.authService.isLoggedIn() && this.authService.getRole() === 'USER') {
+      return true;
+    }
+    
+    // Redirect to login or unauthorized page if not an admin
+    this.router.navigate(['']); // or '/unauthorized'
+    return false;
+  }
+}
