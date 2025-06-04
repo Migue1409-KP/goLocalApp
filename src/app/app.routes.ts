ca15faa7c85a4e017d78883060b3d4ed1bfd8dd1
Andres Ochoa <andresochoaflorez0@gmail.com>
2025-06-04 15:15:44 -0500

    feature(routes): add new routes for home, business profile, and experience management

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,6 +12,11 @@ import { PerfilComponent } from './users/perfil/perfil.component';
 import { AdminPanelComponent } from './admin/dashboard/admin-panel.component';
 import { RegisterBusinessComponent } from './business/register/register.component';
 import { BusinessListComponent } from './business/list/business-list.component';
+import { ProfileBusinessComponent } from './business/profile/profile-business.component';
+import { RegisterExperienceComponent } from './experience/register/register-experience.component';
+import { ExperienceListComponent } from './experience/list/experience-list.component';
+import { ProfileExperienceComponent } from './experience/profile/profile-experience.component';
+import { HomeComponent } from './home/home.component';
 
 
 export const routes: Routes = [
@@ -24,6 +29,11 @@ export const routes: Routes = [
         path: 'login',
         title: 'Iniciar Sesión',
         component: LoginComponent
+    },
+    {
+    path: 'home',
+    title: 'Inicio',
+    component: HomeComponent
     },
     {
         path: 'favorites',
@@ -54,6 +64,26 @@ export const routes: Routes = [
         title: 'Listar Negocios',
         component: BusinessListComponent,
         // canActivate: [UserAuthGuard] // O la que prefieras, puedes omitir si es público
+    },
+    {
+        path: 'profileBussiness/:id',
+        title: 'Perfil de Negocio',
+        component: ProfileBusinessComponent
+    },
+    {
+    path: 'register-experience',
+    title: 'Registrar experiencia',
+    component: RegisterExperienceComponent
+    },
+    {
+    path: 'list-experiences',
+    title: 'Experiencias disponibles',
+    component: ExperienceListComponent
+    },
+    {
+    path: 'profileExperience/:id',
+    title: 'Perfil de experiencia',
+    component: ProfileExperienceComponent
     },
     {
         path: '**',
